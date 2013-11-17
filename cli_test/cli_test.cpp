9002afe45f234569767f4a8b6e7c4bef48f1cf95
Jan Ekström <jeebjp@gmail.com>
2013-11-17 22:53:11 +0200

    cli_test: Add Windows bitmap creation/usage as well as BMP writing

--- cli_test/cli_test.cpp
+++ cli_test/cli_test.cpp
@@ -4,12 +4,79 @@
 
 #include <stdint.h>
 
+// HBITMAP stuff
+#include <windows.h>
+#include <wingdi.h>
+
 extern "C" {
 #include <libavutil/imgutils.h>
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 
 #include <libswscale/swscale.h>
+}
+
+// Picked off the internets for quick testing
+void SaveBitmap(char *szFilename, HBITMAP hBitmap)
+{
+    HDC                 hdc = NULL;
+    FILE*               fp = NULL;
+    LPVOID              pBuf = NULL;
+    BITMAPINFO          bmpInfo;
+    BITMAPFILEHEADER    bmpFileHeader;
+
+    do{
+
+        hdc = GetDC(NULL);
+
+        ZeroMemory(&bmpInfo, sizeof(BITMAPINFO));
+
+        bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+
+        GetDIBits(hdc, hBitmap, 0, 0, NULL, &bmpInfo, DIB_RGB_COLORS);
+
+        if (bmpInfo.bmiHeader.biSizeImage <= 0)
+            bmpInfo.bmiHeader.biSizeImage = bmpInfo.bmiHeader.biWidth*abs(bmpInfo.bmiHeader.biHeight)*(bmpInfo.bmiHeader.biBitCount + 7) / 8;
+
+        if ((pBuf = malloc(bmpInfo.bmiHeader.biSizeImage)) == NULL)
+        {
+            MessageBox(NULL, "Unable to Allocate Bitmap Memory", "Error", MB_OK | MB_ICONERROR);
+            break;
+        }
+
+        bmpInfo.bmiHeader.biCompression = BI_RGB;
+
+        GetDIBits(hdc, hBitmap, 0, bmpInfo.bmiHeader.biHeight, pBuf, &bmpInfo, DIB_RGB_COLORS);
+
+        if ((fp = fopen(szFilename, "wb")) == NULL)
+        {
+            MessageBox(NULL, "Unable to Create Bitmap File", "Error", MB_OK | MB_ICONERROR);
+            break;
+        }
+
+        bmpFileHeader.bfReserved1 = 0;
+
+        bmpFileHeader.bfReserved2 = 0;
+
+        bmpFileHeader.bfSize = sizeof(BITMAPFILEHEADER)+sizeof(BITMAPINFOHEADER)+bmpInfo.bmiHeader.biSizeImage;
+
+        bmpFileHeader.bfType = 'MB';
+
+        bmpFileHeader.bfOffBits = sizeof(BITMAPFILEHEADER)+sizeof(BITMAPINFOHEADER);
+
+        fwrite(&bmpFileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+
+        fwrite(&bmpInfo.bmiHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+
+        fwrite(pBuf, bmpInfo.bmiHeader.biSizeImage, 1, fp);
+
+    } while (false);
+
+    if (hdc)     ReleaseDC(NULL, hdc);
+
+    if (pBuf)    free(pBuf);
+
+    if (fp)      fclose(fp);
 }
 
 int main(int argc, char **argv)
@@ -19,12 +86,14 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    /*
     FILE *dst_file;
     dst_file = fopen(argv[2], "wb");
     if (!dst_file) {
-        fprintf(stderr, "Failed to open output file %s\n", argv[2]);
-        return 1;
+    fprintf(stderr, "Failed to open output file %s\n", argv[2]);
+    return 1;
     }
+    */
 
     fprintf(stderr, "Success: Opened output file %s\n", argv[2]);
 
@@ -146,44 +215,85 @@ int main(int argc, char **argv)
     fprintf(stderr, "Stream SAR: %d:%d\n", frame->sample_aspect_ratio.num, frame->sample_aspect_ratio.den);
     fprintf(stderr, "Guessed SAR: %d:%d\n", guessed_sar.num, guessed_sar.den);
 
-    int dst_width  = 0;
+    int dst_width = 0;
     int dst_height = 0;
 
     if (guessed_sar.num > guessed_sar.den) {
-        dst_width  = (frame->width * guessed_sar.num) / guessed_sar.den;
+        dst_width = (frame->width * guessed_sar.num) / guessed_sar.den;
         dst_height = frame->height;
-    } else {
+    }
+    else {
         dst_height = (frame->height * guessed_sar.den) / guessed_sar.num;
-        dst_width  = frame->width;
+        dst_width = frame->width;
     }
 
     fprintf(stderr, "DSTWidth: %d , DSTHeight: %d\n", dst_width, dst_height);
-    /*
-    // Allocate an AVPicture for the RGB version
-    AVPicture pic;
-    ret = avpicture_alloc(&pic, AV_PIX_FMT_RGB32, frame->width, frame->height);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to allocate the output RGB picture :<\n");
-        return 1;
+
+#define LIMITED_SIZE 128
+
+    // Fit the thing into 32x32 (smallest thumbnail size)
+    if (dst_width > dst_height) {
+        double ratio = (double)dst_height / (double)dst_width;
+        dst_width = LIMITED_SIZE;
+        dst_height = (int)((ratio * (double)LIMITED_SIZE) + 0.5);
     }
-    */
-
-    uint8_t *dst_data[4];
-    int dst_linesize[4];
-
-    ret = av_image_alloc(dst_data, dst_linesize, dst_width, dst_height, AV_PIX_FMT_RGBA, 1);
-    if (ret < 0) {
-        fprintf(stderr, "Failed to allocate the output RGB picture :<\n");
-        return 1;
+    else {
+        double ratio = (double)dst_width / (double)dst_height;
+        dst_height = LIMITED_SIZE;
+        dst_width = (int)((ratio * (double)LIMITED_SIZE) + 0.5);
     }
 
-    int dst_image_size = ret;
+    fprintf(stderr, "DSTWidth: %d , DSTHeight: %d (post-fitting)\n", dst_width, dst_height);
+
+    uint8_t *dst_data[4] = { nullptr };
+    int      dst_linesize[4] = { 0 };
+
+    // The linesize is padded to the next 4 byte alignment
+    int how_much_over = dst_width % 4;
+    if (how_much_over) {
+        dst_linesize[0] = dst_width + (4 - how_much_over);
+    } else {
+        dst_linesize[0] = dst_width;
+    }
+
+    fprintf(stderr, "Linesize: %d\n", dst_linesize[0]);
+
+    // Create a BITMAPINFO structure to create the bitmap with
+    BITMAPINFO bmi;
+    ZeroMemory(&bmi, sizeof(bmi));
+
+    // Set the values to the header
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+    bmi.bmiHeader.biWidth  = dst_width;
+    bmi.bmiHeader.biHeight = -dst_height;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+    bmi.bmiHeader.biSizeImage = 0;
+    bmi.bmiHeader.biXPelsPerMeter = 0;
+    bmi.bmiHeader.biYPelsPerMeter = 0;
+    bmi.bmiHeader.biClrUsed = 0;
+    bmi.bmiHeader.biClrImportant = 0;
+
+    // Get the screen... whyyyyy
+    HDC hdc = GetDC(NULL);
+
+    // Create a Windows HBITMAP bitmap
+    HBITMAP dst_bitmap = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, (void **)dst_data, NULL, 0);
+    if (!dst_bitmap || !dst_data) {
+        fprintf(stderr, "Failed to create the HBITMAP :<\n");
+        return 1;
+    }
+
+    if ((long)dst_bitmap == ERROR_INVALID_PARAMETER) {
+        fprintf(stderr, "Invalid parameters with the DIBSection, ho!\n");
+    }
 
     fprintf(stderr, "Success: The output RGB picture has been allocated\n");
 
     SwsContext *swscale_context = sws_getContext(frame->width, frame->height, (AVPixelFormat)frame->format,
-                                                 dst_width, dst_height, AV_PIX_FMT_RGBA,
-                                                 SWS_BICUBIC, NULL, NULL, NULL);
+                                                 dst_width, dst_height, AV_PIX_FMT_RGB32,
+                                                 SWS_BILINEAR, NULL, NULL, NULL);
     if (!swscale_context) {
         fprintf(stderr, "Failed to create the swscale context for the YCbCr->RGB conversion\n");
         return 1;
@@ -199,10 +309,15 @@ int main(int argc, char **argv)
 
     fprintf(stderr, "Success: Picture has been scaled\n");
 
-    fwrite(dst_data[0], 1, dst_image_size, dst_file);
-    fclose(dst_file);
+    SaveBitmap(argv[2], dst_bitmap);
+
+    //fwrite(dst_data[0], 1, dst_image_size, dst_file);
+    //fclose(dst_file);
 
     // I think we're done? Add resizing to aspect ratio and that's it?
+
+    // We don't need the stinking screen
+    ReleaseDC(NULL, hdc);
 
     return 0;
 }
